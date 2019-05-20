5dd526889cf263b65eebcd3f3d046194d0279711
liam619 <s3697652@student.rmit.edu.au>
2019-05-20 23:19:58 +1000

    Update contact us minor details

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -5,10 +5,10 @@ class ContactsController < ApplicationController
 
     if @contact.valid?
       UserMailer.contact_us(@contact).deliver_now
-      flash[:success] = "Email sent!"
+      flash[:success] = "Your inquiries/feedback have been sent to the site of admin. Thank you."
       redirect_back(fallback_location: root_path)
     else
-      flash[:danger] = "invalid"
+      flash[:danger] = "Failed to send message, please try again."
       redirect_back(fallback_location: root_path)
     end
   end
