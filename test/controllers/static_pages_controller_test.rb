ec18cd542cf69f85d45299ed99a9e0d69018a678
Syed Hariz <s3701799@student.rmit.edu.au>
2019-05-21 15:44:19 +0000

    Updated static pages tests

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -14,25 +14,5 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
     assert_select "title", "Home | #{@base_title}"
   end
-
-  test "should get locations" do
-#    get static_pages_locations_url
-    get locations_path
-    assert_response :success
-    assert_select "title", "Locations | #{@base_title}"
-  end
   
-  test "should get allcourses" do
-#    get static_pages_allcourses_url
-    get allcourses_path
-    assert_response :success
-    assert_select "title", "All Courses | #{@base_title}"
-  end
-
-  test "should get allcoordinators" do
-#    get static_pages_allcoordinators_url
-    get allcoordinators_path
-    assert_response :success
-    assert_select "title", "All Coordinators | #{@base_title}"
-  end
 end
