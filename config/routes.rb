45faec61ee11104071b644370b64cfcb66561d5b
Syed Hariz <s3701799@student.rmit.edu.au>
2019-05-22 15:28:20 +0000

    Commented locations path

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,18 @@ Rails.application.routes.draw do
 
   root 'static_pages#home'
 
+  #sign up
   get '/signup', to: 'users#new'
   post '/signup', to: 'users#create'
-
+  
+  #log in
   get '/login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
+  
+  #location
+  #get '/locations', to: 'locations#new'
+  #post '/locations', to: 'locations#create'
 
   # route for like/dislike course
   post   'courses/:id/likecourse',     to: 'likecourses#new', as: 'likecourse'
@@ -22,7 +28,7 @@ Rails.application.routes.draw do
   get '/422', to: 'errors#unprocessable', :via => :all
   get '/500', to: 'errors#internal_server_error', :via => :all
 
-  resources :users, except: [:new, :create]
+  resources :users#, except: [:new, :create]
   resources :courses
   resources :categories
   resources :locations
