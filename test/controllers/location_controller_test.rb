1359d052f4391c1184513dd0197503dc1f2fb0ba
Syed Hariz <s3701799@student.rmit.edu.au>
2019-05-22 15:26:51 +0000

    Failed tests for locations

--- test/controllers/location_controller_test.rb
+++ test/controllers/location_controller_test.rb
@@ -1,8 +1,20 @@
 require 'test_helper'
 
 class LocationControllerTest < ActionDispatch::IntegrationTest
+  #include Devise::Test::IntegrationHelpers
+
+  setup do
+    @user = users(:example)
+    log_in_as(@user)
+  end
+
+  test "should get index" do
+    get locations_url
+    assert_response :success
+  end
+
   test "should get new" do
-    get location_new_url
+    get locations_path
     assert_response :success
   end
 
