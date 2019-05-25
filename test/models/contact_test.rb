bcd5e4740865d82aee8abc832e4489ad135b0574
Syed Hariz <s3701799@student.rmit.edu.au>
2019-05-25 13:34:08 +0000

    Complete contact model  test

--- test/models/contact_test.rb
+++ test/models/contact_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class ContactTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @contact = Contact.new(name: "Example User", email: "user@example.com",
+                     message: "This is a message")
+  end
+  
+  test "should be valid" do
+    assert @contact.valid?
+  end
+  
+  test "email should be present" do
+    @contact.email = "     "
+    assert_not @contact.valid?
+  end
+  
+  test "message should be present" do
+    @contact.message = "     "
+    assert_not @contact.valid?
+  end
+  
 end
