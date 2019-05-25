26a18eefe4459fefb4fa5d28ced3b727bccec47b
Syed Hariz <s3701799@student.rmit.edu.au>
2019-05-25 13:27:38 +0000

    Updated course.rb model

--- app/models/course.rb
+++ app/models/course.rb
@@ -16,8 +16,6 @@ class Course < ApplicationRecord
   mount_uploader :image, ImageUploader
   validate :image_size
 
-  private
-
   def image_size
     if image.size > 5.megabytes
       errors.add(:image, "should be less than 5MB")
