b813c23320cfe01530dff0f8d79fb9c9dfe2dcbd
Syed Hariz <s3701799@student.rmit.edu.au>
2019-05-25 15:30:05 +0000

    Complete user mailers test

--- test/mailers/user_mailer_test.rb
+++ test/mailers/user_mailer_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class UserMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "mailer" do
+    contact = Contact.new(name: "Example User", email: "user@example.com", message: "This is a message")
+    mail = UserMailer.contact_us(contact)
+    assert_equal "YourApp Contact Us", mail.subject
+    assert_equal ["s3697652@student.rmit.edu.au"], mail.to
+    assert_equal ["noreply@yourapp.com"], mail.from
+  end
+  
 end
