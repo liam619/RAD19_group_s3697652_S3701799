f065abc4cd6a602dca848c385602fc3f495a2aa6
Syed Hariz <s3701799@student.rmit.edu.au>
2019-05-25 14:32:17 +0000

    Complete dislike courses controller test

--- test/controllers/dislike_course_controller_test.rb
+++ test/controllers/dislike_course_controller_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class DislikeCourseControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:example)
+    log_in_as(@user)
+    @course = courses(:one)
+    @dislike = dislike_courses(:one)
+  end
+  
+  test "like should be valid" do
+      assert_not @dislike.valid?
+  end
 end
