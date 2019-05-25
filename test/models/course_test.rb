e9dec89a5771ee428396454be197686c011c3f99
Syed Hariz <s3701799@student.rmit.edu.au>
2019-05-25 13:25:18 +0000

    Complete course model test

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class CourseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+    @course = courses(:one)
+  end
+  test "image should not be too large" do
+        @course.image
+        assert_not @course.valid?
+    end
 end
