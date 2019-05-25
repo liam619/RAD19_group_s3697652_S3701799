ae1f58316e0a2fe8a8744044f338050089d2b7a3
Syed Hariz <s3701799@student.rmit.edu.au>
2019-05-25 14:02:06 +0000

    Complete prerequisite model test

--- test/models/prerequisite_test.rb
+++ test/models/prerequisite_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class PrerequisiteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @prerequisite = courses(:one)
+  end
+  
+  test "name should be present" do
+        @prerequisite.name
+        assert_not @prerequisite.valid?
+  end
 end
