e7b17ac55e4007940b074511fc223e3d9546cc4f
Syed Hariz <s3701799@student.rmit.edu.au>
2019-05-25 13:44:18 +0000

    Updated course test

--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -5,8 +5,19 @@ class CourseTest < ActiveSupport::TestCase
   def setup
     @course = courses(:one)
   end
+  
   test "image should not be too large" do
         @course.image
         assert_not @course.valid?
-    end
+  end
+  
+  test "name should be present" do
+    @course.name = "     "
+    assert_not @course.valid?
+  end
+  
+  test "description should be present" do
+    @course.description = "     "
+    assert_not @course.valid?
+  end
 end
