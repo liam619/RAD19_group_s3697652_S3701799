0907bc8f3b45830bb386a5513faa7d010c08bc98
Syed Hariz <s3701799@student.rmit.edu.au>
2019-05-25 18:44:25 +0000

    Commented line 24 in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,7 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true
 
   has_secure_password
+  #validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   # Returns the hash digest of the given string.
   def self.digest(string)
